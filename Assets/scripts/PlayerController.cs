479e6f2ad4f19a11c98f86dbe62d37817988cf73
ruchit <rhpatel1605@gmail.com>
2019-10-09 15:56:54 -0400

    //

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     float speed = 10;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,14 +19,12 @@ public class PlayerController : MonoBehaviour
         float h, v, f;
 
         h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
+       // v = Input.GetAxis("Vertical");
 
         //Debug.Log("Horizontal : "+ h + "Vertical : " + v);
 
         Rigidbody2D rBody = GetComponent<Rigidbody2D>();
         rBody.velocity = new Vector2(h * speed, v * speed);
-
-
 
     }
 }
