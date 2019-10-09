acf812b72c1322c477cbea1e3da5d08a99498995
ruchit <rhpatel1605@gmail.com>
2019-10-09 15:50:15 -0400

    added initial files

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float h, v, f;
+
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        //Debug.Log("Horizontal : "+ h + "Vertical : " + v);
+
+        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+        rBody.velocity = new Vector2(h * speed, v * speed);
+
+
+
+    }
+}
---
+++ Assets/sprits/playercontrolar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrolar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Fixedupdate is used when modifying physics value
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horiz=Input.GetAxis("Horizontal");
+        float vert= Input.GetAxis("Vertical");
+
+       // Debug.Log("Horizontal: " + horiz);
+       Rigidbody2D rbody= GetComponent<Rigidbody2D>();
+       
+
+    }
+}
